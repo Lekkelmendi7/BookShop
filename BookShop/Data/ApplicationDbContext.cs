d13f2f25b3110a8e28c6b3f31c6cf34da48e8da5
Lekkelmendi7 <95182111+Lekkelmendi7@users.noreply.github.com>
2024-07-04 09:40:53 +0200

    first commit

---
+++ BookShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BookShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShop.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        DbSet<Category> Categories { get; set; }    
+
+
+    }
+}
